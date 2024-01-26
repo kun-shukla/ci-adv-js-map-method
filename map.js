c048ef0462f068509d594517eb2a9d70a61d1d80
kunalshukla <kun.shukla@gmail.com>
2024-01-26 16:16:48 +0000

    multiply numbers by 2 challenge

--- map.js
+++ map.js
@@ -1,39 +1,39 @@
-  
 /**
- * To run this file in Gitpod, use the 
+ * To run this file in Gitpod, use the
  * command node map.js in the terminal
  */
 
 // Using a for loop
 
-
 // Using map()
-
 
 // Simplified w/ map()
 
-
 // Simplfied w/ map() + arrow function
-
 
 // With objects:
 const students = [
   {
     id: 1,
-    name: 'Mark',
-    profession: 'Developer',
-    skill: 'JavaScript'
+    name: "Mark",
+    profession: "Developer",
+    skill: "JavaScript",
   },
   {
     id: 2,
-    name: 'Ariel',
-    profession: 'Developer',
-    skill: 'HTML'
+    name: "Ariel",
+    profession: "Developer",
+    skill: "HTML",
   },
   {
     id: 3,
-    name: 'Jason',
-    profession: 'Designer',
-    skill: 'CSS'
+    name: "Jason",
+    profession: "Designer",
+    skill: "CSS",
   },
 ];
+
+let nums = [1, 2, 3, 4, 5];
+
+let multiplyByTwo = nums.map((num) => num * 2);
+console.log(multiplyByTwo);
